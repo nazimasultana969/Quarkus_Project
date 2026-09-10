bd8e2f879c58e48bb9f9c7d92191b52b3cf713aa
nazimasultana969 <nazimasultana969@gmail.com>
2026-09-10 12:57:19 +0530

    Delete kafka-saga-demo directory
    Changes are for kafka not Quarkus

--- kafka-saga-demo/src/main/java/com/example/saga/exception/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.example.saga.exception;
-
-public class BusinessException extends RuntimeException {
-
-	public BusinessException(String message) {
-
-		super(message);
-	}
-}
--- kafka-saga-demo/src/main/java/com/example/saga/inventory/repository/InventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.saga.inventory.repository;
-
-import com.example.saga.inventory.entity.Inventory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-}
