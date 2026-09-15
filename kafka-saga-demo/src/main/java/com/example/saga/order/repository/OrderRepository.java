bc184a5d125fa359d94a403c23b01d6faf2b338f
nazimasultana969 <nazimasultana969@gmail.com>
2026-09-15 16:35:45 +0530

    Delete kafka-saga-demo directory
    got in different project

--- kafka-saga-demo/src/main/java/com/example/saga/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.saga.order.repository;
-
-import com.example.saga.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- kafka-saga-demo/src/main/java/com/example/saga/payment/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.saga.payment.repository;
-
-import com.example.saga.payment.entity.Payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
